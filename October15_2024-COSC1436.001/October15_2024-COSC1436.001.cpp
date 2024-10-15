f72231c30efb32a82250a78eea5edb69cdbc380e
Work <Work@MSI>
2024-10-15 10:11:19 -0500

    Add project files.

---
+++ October15_2024-COSC1436.001/October15_2024-COSC1436.001.cpp
@@ -0,0 +1,9 @@
+// October15_2024-COSC1436.001.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
